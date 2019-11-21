a4ed8201905db57f4b7a27a8f4aff5fb1a187924
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2019-11-21 09:55:29 -0700

    Fixed bug in span onClick logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -225,13 +225,10 @@ function clearErrModal() {
 // Modal code
 //
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    clearErrModal();
-}
+$(document.body).on('click', '.close', function () {
+    modal.style.display = "none";
+    $("#search").val("");
+});
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
